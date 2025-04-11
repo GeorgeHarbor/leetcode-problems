89448f428967e35140e9687405dde55dc8e35f27
GeorgeHarbor <ldjordjevic@protonmail.com>
2025-04-11 15:23:42 +0200

    palindrome-number First Try

---
+++ palindrome-number/Program.cs
@@ -0,0 +1,23 @@
+﻿bool IsPalindrome(int num)
+{
+  int y = 0;
+  int x = num;
+
+  if (x < 0)
+    return false;
+
+
+  while (x > 0)
+  {
+    y *= 10;
+    y += x % 10;
+    x /= 10;
+  }
+
+  if (num == y)
+    return true;
+
+  return false;
+}
+
+System.Console.WriteLine(IsPalindrome(-121));
